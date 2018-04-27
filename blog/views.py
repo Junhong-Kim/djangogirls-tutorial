b814ddb94fb76b2540ee7589a1590f61b1cfafa0
Junhong-Kim <jhk1005jhk@gmail.com>
2018-04-27 17:26:26 +0900

    add: django template

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Post
 
 
 def post_list(request):
+    posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('published_date')
     # Template Loader
     # blog/templates/blog/post_list.html
-    return render(request, 'blog/post_list.html')
+    return render(request, 'blog/post_list.html', {
+        'posts': posts,
+    })
 
 
 def post_detail(request, pk):
