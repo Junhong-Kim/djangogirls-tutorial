44b1e292d3848533c09013bd8159defcddfe173c
Junhong-Kim <jhk1005jhk@gmail.com>
2018-04-27 15:00:48 +0900

    add: blog urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def post_list(request):
+    return HttpResponse('post_list')
+
+
+def post_detail(request, pk):
+    return HttpResponse('post_detail: {}'.format(pk))
